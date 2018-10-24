566befcfbed7fa26fb07db2ea52c08e026401ab7
Toni Robres <a.robres@scrm.edu>
2018-10-24 22:05:41 +0200

    change id

--- microblog/solution/tests/user_information.py
+++ microblog/solution/tests/user_information.py
@@ -20,21 +20,21 @@ class TestGetUserInformation():
 
     def test_get_user_information(self):
         headers = self.get_token()
-        response = self.microblog.get_user_information(headers=headers, user_id=2)
+        response = self.microblog.get_user_information(headers=headers, user_id=1)
         assert_true(response.ok)
         json_body = response.json()
         assert_equals(json_body['username'], 'twiindan')
 
     def test_get_user_information_with_invalid_token(self):
         headers = {"Authorization": "Bearer {}".format('not valid')}
-        response = self.microblog.get_user_information(headers=headers, user_id=2)
+        response = self.microblog.get_user_information(headers=headers, user_id=1)
         assert_equals(401, response.status_code)
 
     def test_modify_profile(self):
         headers = self.get_token()
         headers['Content-Type'] = 'application/json'
         body = ModifyUser(username='twiindan', email='twiindan@gmail.com', about_me='API Expert')
-        response = self.microblog.modify_user_information(headers=headers, user_id=2, body=body.to_dict())
+        response = self.microblog.modify_user_information(headers=headers, user_id=1, body=body.to_dict())
         print(response.status_code)
         print(response.json())
         assert_true(response.ok)
